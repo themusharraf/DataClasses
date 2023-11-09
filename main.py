9988d0a9684094d0d5572509836898895acd62f6
allnc <meibrohimov@gmail.com>
2023-11-09 07:30:50 +0500

    Initial commit 📗

---
+++ main.py
@@ -0,0 +1,65 @@
+# A basic Data Class
+# importing dataclass module
+# from dataclasses import dataclass
+#
+# @dataclass
+# class employee:
+#     # Attributes  using Type Hints
+#     name: str
+#     emp_id: str
+#     age: int
+#     city: str
+#
+#
+# emp1 = employee("Omina", "ans24", 21, 'samarkand')
+# emp2 = employee("Akbar", "auth23", 28, 'bukhara')
+# emp3 = employee("Zilola", "zilol18", 21, 'Tashkent')
+#
+# print("xodimlar ob'ekti :")
+# print(emp1)
+# print(emp2)
+# print(emp3)
+#
+# # referring two object to check equality
+# print("emp1 va emp2 ma'lumotlari bir xilmi? ", emp1 == emp2)
+# print("emp1 va emp3 ma'lumotlari bir xilmi?", emp1 == emp3)
+
+from dataclasses import dataclass
+
+
+#
+#
+# A class for holding an employees content
+@dataclass
+class employee:
+    # Attributes Declaration
+    # using Type Hints
+    name: str
+    emp_id: str
+    age: int
+    city: str
+
+
+# object of the class
+emp = employee("Musharraf", "allnc9", 23, 'Tashkent')
+
+print(emp.__dataclass_fields__)
+
+# default field example
+# from dataclasses import dataclass, field
+# @dataclass
+# class employee:
+#     # Attributes Declaration
+#     # using Type Hints
+#     name: str
+#     emp_id: str
+#     age: int
+#
+#     # default field set
+#     # city : str = "patna"
+#     city: str = field(default="Tashkent")
+#
+# emp = employee("Dilshod", "dilshod001", 21)
+# print(emp)
+# # output
+# # employee(name='Dilshod', emp_id='dilshod001', age=21, city='Tashkent')
