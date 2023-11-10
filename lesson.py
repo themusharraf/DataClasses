cbc6764cee14867c9015826e386c7f09f6443dd5
allnc <meibrohimov@gmail.com>
2023-11-10 14:03:20 +0500

    new change 📗

---
+++ lesson.py
@@ -0,0 +1,8 @@
+import modul as func
+
+print(dir(func))
+
+# output
+# ['User', 'Users', '__builtins__', '__cached__',
+# '__doc__', '__file__', '__loader__', '__name__',
+# '__package__', '__spec__']
---
+++ modul.py
@@ -0,0 +1,23 @@
+User = {
+    "name": "Akbar",
+    "username": "@akbar",
+    "password": "@akbar09",
+    "email": "akbarb@gmail.com",
+}
+
+
+class Users:
+    def __init__(self, name, username, password, email):
+        self.name = "Akbar"
+        self.username = "@akbar"
+        self.password = "all7889"
+        self.email = "all7889@gmail"
+
+    def get_username(self):
+        return self.username
+
+    def get_email(self):
+        return self.email
+
+    def get_password(self):
+        return self.password
