dccea6567cb8beaf586b64f02c9d2e9b619f9dc9
allnc <meibrohimov@gmail.com>
2023-11-12 01:38:11 +0500

    new test file  📗

--- lesson.py
+++ lesson.py
@@ -11,7 +11,7 @@ class TestMathOperations(unittest.TestCase):
 
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
-        self.assertEqual(subtract(-2, -3), 1)
+        self.assertEqual(subtract(-2, -3),1)
 
     def test_multiply(self):
         self.assertEqual(multiply(2, 3), 6)
